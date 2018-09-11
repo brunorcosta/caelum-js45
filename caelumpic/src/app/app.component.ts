569de86590a471f5c890f8226c8b250d383a790a
Bruno Ribeiro Costa <brcosta@macbook.local>
2018-09-10 21:59:18 -0300

    Ëxercícios até o capítulo

--- caelumpic/src/app/app.component.ts
+++ caelumpic/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'caelumpic';
+  title = 'Caelumpic';
+  listaFotos;
+
+  constructor(http: HttpClient) {
+
+    http.get('http://localhost:3000/v1/fotos')
+      .subscribe(fotosApi => this.listaFotos = fotosApi, erro => console.error(erro)
+      );
+  }
 }
